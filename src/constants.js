fc71f43004e71b187d176f1b142aec1b9a5d1568
Konstantin <you@example.com>
2021-10-26 20:39:11 +0300

    Make server

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,11 @@ module.exports.ExitCode = {
 };
 
 module.exports.DATE_PERIOD = 3;
+
+module.exports.HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
