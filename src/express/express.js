2ea5f79c3948160f16df98f438542a03540cc2d1
Konstantin <you@example.com>
2022-02-16 18:48:27 +0300

    Make pug templates

--- src/express/express.js
+++ src/express/express.js
@@ -6,10 +6,17 @@ const articlesRoutes = require(`./routes/articles-routes`);
 const categoriesRoutes = require(`./routes/categories-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
+const path = require(`path`);
 
+const PUBLIC_DIR = `public`;
+const TEMPLATE_DIR = `templates`;
 const DEFAULT_PORT = 8080;
 
 const app = express();
+app.set(`views`, path.resolve(__dirname, TEMPLATE_DIR));
+app.set(`view engine`, `pug`);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/articles`, articlesRoutes);
 app.use(`/categories`, categoriesRoutes);
