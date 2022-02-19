caa32a49bb9dc7c2e0d57ed606e424858a2873e2
Konstantin <you@example.com>
2022-02-19 22:42:07 +0300

    Fix Б33; make optimisation

--- src/utils.js
+++ src/utils.js
@@ -2,25 +2,28 @@
 
 const {DATE_PERIOD} = require(`./constants`);
 
-module.exports.getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-module.exports.shuffle = (someArray) => {
-  for (let i = someArray.length - 1; i > 0; i--) {
+const shuffle = (someArray) => {
+  const array = [...someArray];
+  for (let i = array.length - 1; i > 0; i--) {
     const randomPosition = Math.floor(Math.random() * i);
-    [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
+    [array[i], array[randomPosition]] = [array[randomPosition], array[i]];
   }
 
-  return someArray;
+  return array;
 };
 
-module.exports.getRandomDate = () => {
+const getRandomDate = () => {
   const start = new Date();
   const end = new Date();
   end.setMonth(start.getMonth() - DATE_PERIOD);
 
   return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 };
+
+module.exports = {getRandomInt, shuffle, getRandomDate}
